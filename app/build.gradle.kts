d75c9f55935d349a13265d6c5914b4dbb5e93e9b
Lars Eckart <lars.eckart@hey.com>
2025-04-22 09:05:35 +0200

    getting the jar file to work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,33 @@ java {
 }
 
 application {
-    mainClass = "org.example.App"
+    mainClass = "com.larseckart.App"
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+}
+
+// Set the Main-Class attribute in the jar manifest
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+// Fat JAR task to include all dependencies
+
+tasks.register<Jar>("fatJar") {
+    archiveClassifier.set("all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith(".jar") }.map { zipTree(it) }
+    })
+}
